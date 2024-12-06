5f29b76fdc7401ca7c93076bd6de70397b818d43
theayushkumar <ayushkumarhjp@gmail.com>
2024-12-06 17:22:26 +0530

    video design done

--- src/app/video-player/video-player.component.ts
+++ src/app/video-player/video-player.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-video-player',
@@ -7,9 +7,79 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoPlayerComponent implements OnInit {
 
-  constructor() {}
+  @ViewChild('videoPlayer', { static: false }) videoPlayer!: ElementRef<HTMLVideoElement>;
+  currentTime: number = 0;
+  duration: number = 0;
+  isPlaying: boolean = false; // Track whether the video is playing or paused
 
-  ngOnInit() {}
+
+  @ViewChild('popover') popover!: { event: Event; };
+
+  isOpen = false;
+
+  presentPopover(e: Event) {
+    this.popover.event = e;
+    this.isOpen = true;
+  }
 
   
+
+  constructor() { }
+
+  ngOnInit() { }
+
+  ngAfterViewInit() {
+    const video = this.videoPlayer.nativeElement;
+    video.addEventListener('timeupdate', this.updateVideoTime.bind(this));
+    video.addEventListener('loadedmetadata', this.setVideoDuration.bind(this));
+    video.addEventListener('play', this.onPlay.bind(this));   // Event listener for play
+    video.addEventListener('pause', this.onPause.bind(this)); // Event listener for pause
+  }
+
+  // Update currentTime as the video plays
+  updateVideoTime() {
+    this.currentTime = this.videoPlayer.nativeElement.currentTime;
+  }
+
+  // Set video duration once the metadata is loaded
+  setVideoDuration() {
+    this.duration = this.videoPlayer.nativeElement.duration;
+  }
+
+  // Update play state when the video starts playing
+  onPlay() {
+    this.isPlaying = true;
+  }
+
+  // Update play state when the video is paused
+  onPause() {
+    this.isPlaying = false;
+  }
+
+  // Play or Pause the video
+  togglePlay() {
+    const video = this.videoPlayer.nativeElement;
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  }
+
+  // Skip backward by 10 seconds
+  skipBackward() {
+    const video = this.videoPlayer.nativeElement;
+    video.currentTime -= 10;
+  }
+
+  // Skip forward by 10 seconds
+  skipForward() {
+    const video = this.videoPlayer.nativeElement;
+    video.currentTime += 10;
+  }
+
+  onRangeChange(event: any) {
+    const video = this.videoPlayer.nativeElement;
+    video.currentTime = event.detail.value;
+  }
 }
