c5fae903b94d8eb80d8e324ff717c2b59b845a06
theayushkumar <ayushkumarhjp@gmail.com>
2024-12-06 15:41:51 +0530

    video player design done

--- src/app/video-player/video-player.component.ts
+++ src/app/video-player/video-player.component.ts
@@ -6,19 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-player.component.scss'],
 })
 export class VideoPlayerComponent implements OnInit {
-  isPlaying: boolean = true;  // Track the playing state
 
   constructor() {}
 
   ngOnInit() {}
 
-  togglePlayPause(videoElement: HTMLVideoElement) {
-    if (this.isPlaying) {
-      videoElement.pause();  // Pause the video
-    } else {
-      videoElement.play();   // Play the video
-    }
-    this.isPlaying = !this.isPlaying;  // Toggle the play/pause state
-  }
   
 }
